112d30f4ec3cf754589185a3f8968f7b4475e1da
1018528 <laara.rocha@unidavi.edu.br>
2022-10-26 19:56:20 -0300

    controller motorista

--- backend/controller.js
+++ backend/controller.js
@@ -33,6 +33,31 @@ app.get('/obter-lista', async (req, res) => {
 });
 
 
+app.post('/incluir-motorista', async (req, res) => {
+    console.log('teste incluir motorista');
+    if (!(Object.is(req.query.tipo, null)) && !(Object.is(req.query.tipo, ''))) {
+        if (!(Object.is(req.body, null)) && !(Object.is(req.body, ''))) {
+            let result;
+            switch (req.query.tipo) {
+                case 'motorista' :
+                    result = await serviceMotorista.obterListaMotorista(req.body);
+                    break;
+                case 'veiculo' :
+                    result = await serviceVeiculo.obterListaVeiculo(req.body);
+                    break;
+                case 'rota' :
+                    result = await serviceRota.obterListaRota(req.body);
+                    break;
+                case 'despesa' :
+                    result = await serviceDespesa.obterListaDespesa(req.body);
+            }
+
+            res.json(result);
+        }
+    }
+
+});
+
 app.listen(port, () => {
     console.log(`Server on na porta:${port}`);
 });
