bfdca08ed33c37d38057fb7f8111b833587e1285
1018528 <laara.rocha@unidavi.edu.br>
2022-12-01 18:20:03 -0300

    Commit CSS 2

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -3,6 +3,13 @@
     MENU INICIAL
     <form action="http://localhost:8081/backend/controller.js"></form>
   </div>
+  <div>
+    <table>
+      <tr><img src="https://th.bing.com/th/id/OIP.L7ejXaQ6mAR4kUk4eN8EEAHaFi?pid=ImgDet&rs=1"></tr>
+      <tr><img src="https://th.bing.com/th/id/R.3d1a237b88fa5f5dd7edf5c39639b5ba?rik=yQSXcMBF3yip7A&riu=http%3a%2f%2f1.bp.blogspot.com%2f-dAEZFlNg_qk%2fUYVtTEFk9BI%2fAAAAAAACTzk%2fm7Rtgng3of0%2fs1600%2fcarros-imagens%2b(7).jpg&ehk=%2bAaOvO1GgfuJeBhbBDfW5SghTWSulZJT%2f4BaPboPY8c%3d&risl=&pid=ImgRaw&r=0"></tr>
+      <tr><img src="https://quatrorodas.abril.com.br/wp-content/uploads/2020/09/chevrolet_onix_364.jpeg?quality=70&strip=info"></tr>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -17,4 +24,22 @@ export default {
   background-color: lightblue;
 }
 
+div {
+  padding: 5px;
+}
+
+img {
+  width:400px;
+  height: 300px;
+}
+
+img:hover {
+
+}
+
+tr {
+  display: inline;
+  padding: 5px;
+  margin-left: 2px;
+}
 </style>
